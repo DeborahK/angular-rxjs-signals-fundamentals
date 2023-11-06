dec8d7a34f9d1111231ffb97d845a57099671f17
DeborahK <deborahk@insteptech.com>
2023-11-06 13:42:57 -0800

    As of the end of Module 12

--- apm/src/app/products/product.service.ts
+++ apm/src/app/products/product.service.ts
@@ -1,10 +1,11 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
-import { BehaviorSubject, Observable, catchError, combineLatest, filter, map, of, shareReplay, switchMap, tap, throwError } from 'rxjs';
-import { Product } from './product';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, catchError, filter, map, of, shareReplay, switchMap, tap, throwError } from 'rxjs';
+import { Product, Result } from './product';
 import { HttpErrorService } from '../utilities/http-error.service';
 import { ReviewService } from '../reviews/review.service';
 import { Review } from '../reviews/review';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root'
@@ -16,17 +17,32 @@ export class ProductService {
   private errorService = inject(HttpErrorService);
   private reviewService = inject(ReviewService);
 
-  private productSelectedSubject = new BehaviorSubject<number | undefined>(undefined);
-  readonly productSelected$ = this.productSelectedSubject.asObservable();
+  selectedProductId = signal<number | undefined>(undefined);
 
-  readonly products$ = this.http.get<Product[]>(this.productsUrl)
+  private productsResult$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
+      map(p => ({ data: p } as Result<Product[]>)),
       tap(p => console.log(JSON.stringify(p))),
       shareReplay(1),
-      catchError(err => this.handleError(err))
+      catchError(err => of({
+        data: [],
+        error: this.errorService.formatError(err)
+      } as Result<Product[]>))
     );
+  private productsResult = toSignal(this.productsResult$,
+    { initialValue: ({ data: [] } as Result<Product[]>) });
+  products = computed(() => this.productsResult().data);
+  productsError = computed(() => this.productsResult().error);
 
-  readonly product1$ = this.productSelected$
+  // products = computed(() => {
+  //   try {
+  //     return toSignal(this.products$, { initialValue: [] as Product[] })();
+  //   } catch (error) {
+  //     return [] as Product[];
+  //   }
+  // });
+
+  private productResult1$ = toObservable(this.selectedProductId)
     .pipe(
       filter(Boolean),
       switchMap(id => {
@@ -34,25 +50,43 @@ export class ProductService {
         return this.http.get<Product>(productUrl)
           .pipe(
             switchMap(product => this.getProductWithReviews(product)),
-            catchError(err => this.handleError(err))
+            catchError(err => of({
+              data: undefined,
+              error: this.errorService.formatError(err)
+            } as Result<Product>))
           );
-      })
+      }),
+      map(p => ({ data: p } as Result<Product>))
     );
+  
+  // Find the product in the existing array of products
+  private foundProduct = computed(() => {
+    // Dependent signals
+    const p = this.products();
+    const id = this.selectedProductId();
+    if (p && id) {
+      return p.find(product => product.id === id);
+    }
+    return undefined;
+  })
 
-  product$ = combineLatest([
-    this.productSelected$,
-    this.products$
-  ]).pipe(
-    map(([selectedProductId, products]) =>
-      products.find(product => product.id === selectedProductId)
-    ),
-    filter(Boolean),
-    switchMap(product => this.getProductWithReviews(product)),
-    catchError(err => this.handleError(err))
-  );
+  // Get the related set of reviews
+  private productResult$ = toObservable(this.foundProduct)
+    .pipe(
+      filter(Boolean),
+      switchMap(product => this.getProductWithReviews(product)),
+      map(p => ({ data: p } as Result<Product>)),
+      catchError(err => of({
+        data: undefined,
+        error: this.errorService.formatError(err)
+      } as Result<Product>))
+    );
+  private productResult = toSignal(this.productResult$);
+  product = computed(() => this.productResult()?.data);
+  productError = computed(() => this.productResult()?.error);
 
   productSelected(selectedProductId: number): void {
-    this.productSelectedSubject.next(selectedProductId);
+    this.selectedProductId.set(selectedProductId);
   }
 
   private getProductWithReviews(product: Product): Observable<Product> {
@@ -66,9 +100,4 @@ export class ProductService {
     }
   }
 
-  private handleError(err: HttpErrorResponse): Observable<never> {
-    const formattedMessage = this.errorService.formatError(err);
-    return throwError(() => formattedMessage);
-    // throw formattedMessage;
-  }
 }
