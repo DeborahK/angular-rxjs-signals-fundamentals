e24df3d7da5e7d3d22e7db2f0bdb5ebba5e129c5
DeborahK <deborahk@insteptech.com>
2023-11-06 13:27:53 -0800

    As of the end of Module 7

--- apm/src/app/reviews/review.service.ts
+++ apm/src/app/reviews/review.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ReviewService {
-  // Just enough here for the code to compile
   private reviewsUrl = 'api/reviews';
 
   getReviewUrl(productId: number): string {
