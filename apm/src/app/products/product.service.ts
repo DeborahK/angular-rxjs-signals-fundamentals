218b5ee71e43b20348b16060fd8060efecd519cd
DeborahK <deborahk@insteptech.com>
2023-11-06 11:25:06 -0800

    As of the end of Module 7

--- apm/src/app/products/product.service.ts
+++ apm/src/app/products/product.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, catchError, tap, throwError } from 'rxjs';
+import { Observable, catchError, map, of, switchMap, tap, throwError } from 'rxjs';
 import { Product } from './product';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,7 @@ export class ProductService {
 
   private http = inject(HttpClient);
   private errorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
@@ -26,8 +29,20 @@ export class ProductService {
     return this.http.get<Product>(productUrl)
       .pipe(
         tap(() => console.log('In http.get by id pipeline')),
+        switchMap(product => this.getProductWithReviews(product)),
         catchError(err => this.handleError(err))
       );
+  }
+
+  private getProductWithReviews(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(
+          map(reviews => ({ ...product, reviews } as Product))
+        )
+    } else {
+      return of(product);
+    }
   }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
