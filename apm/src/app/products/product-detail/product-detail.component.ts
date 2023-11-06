ff558815d4c5d7adff73d3f08a4e973fc951af75
DeborahK <deborahk@insteptech.com>
2023-11-06 13:35:06 -0800

    As of the end of Module 9

--- apm/src/app/products/product-detail/product-detail.component.ts
+++ apm/src/app/products/product-detail/product-detail.component.ts
@@ -1,48 +1,33 @@
-import { Component, Input, OnChanges, OnDestroy, SimpleChanges, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
-import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
+import { NgIf, NgFor, CurrencyPipe, AsyncPipe } from '@angular/common';
 import { Product } from '../product';
-import { EMPTY, Subscription, catchError } from 'rxjs';
+import { EMPTY, catchError } from 'rxjs';
 import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, CurrencyPipe]
+  imports: [AsyncPipe, NgIf, NgFor, CurrencyPipe]
 })
-export class ProductDetailComponent implements OnChanges, OnDestroy {
-  @Input() productId: number = 0;
+export class ProductDetailComponent {
   errorMessage = '';
-  sub!: Subscription;
 
   private productService = inject(ProductService);
 
   // Product to display
-  product: Product | null = null;
+  product$ = this.productService.product$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
 
   // Set the page title
-  pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const id = changes['productId'].currentValue;
-    if (id) {
-      this.sub = this.productService.getProduct(id)
-        .pipe(
-          catchError(err => {
-            this.errorMessage = err;
-            return EMPTY;
-          })
-        ).subscribe(
-          product => this.product = product
-        );
-    }
-  }
-  ngOnDestroy(): void {
-    if (this.sub) {
-      this.sub.unsubscribe();
-    }
-  }
+  // pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
+  pageTitle = 'ProductDetail';
 
   addToCart(product: Product) {
   }
